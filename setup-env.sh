35e5c2e6e8721125bf49a5c47826f1ee7e3e1cbc
David Boehme <boehme3@llnl.gov>
2023-08-10 10:17:24 -0700

    Update setup-env.sh for radiuss tutorial

--- setup-env.sh
+++ setup-env.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 ROOT_DIR=$(bash -c "cd ${SCRIPT_DIR} && pwd")
 
-_build_config="${1:-default}"
+_build_config="${1:-mpi}"
 _install_dir=${INSTALL_DIR:-"${ROOT_DIR}/install/${_build_config}"}
 
 echo "Build config:      ${_build_config}"
@@ -14,7 +14,7 @@ if [ ! -d "${_install_dir}/bin" ] ; then
     BUILD_CONFIG=${_build_config} INSTALL_DIR=${_install_dir} ${ROOT_DIR}/build.sh
 fi
 
-export PATH=${_install_dir}/bin:${PATH}
+export PATH=${_install_dir}/bin:/usr/lib64/openmpi/bin:${PATH}
 export CMAKE_PREFIX_PATH="${_install_dir};${CMAKE_PREFIX_PATH}"
 
 export BUILD_CONFIG=${_build_config}
